50f696088ee27669a6c033e21aab0e2247d90793
edindzabija <edindzabija@gmail.com>
2020-11-16 00:43:48 +0100

    removed unused dependency

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.scss';
-import React, { Fragment } from 'react';
+import React from 'react';
 import Link from 'next/link';
 import { signIn, signOut, useSession } from 'next-auth/client';
 
