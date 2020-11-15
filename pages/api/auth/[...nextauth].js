8cc7830868fd8f1d48b2b10718edaaa55ed8bf04
edindzabija <edindzabija@gmail.com>
2020-11-15 22:17:57 +0100

    email signup

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from 'next-auth'
-import Providers from 'next-auth/providers'
+import NextAuth from 'next-auth';
+import Providers from 'next-auth/providers';
 
 const options = {
   providers: [
@@ -11,18 +11,23 @@ const options = {
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
     }),
-    // Providers.Email({
-    //   server: {
-    //     host: '',
-    //     port: '',
-    //     auth: {
-    //       user: '',
-    //       pass: '',
-    //     },
-    //   },
-    //   from: '',
-    // }),
+    Providers.Email({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ],
-}
+  database: {
+    type: 'sqlite',
+    database: ':memory:',
+    synchronize: true,
+  },
+};
 
-export default (req, res) => NextAuth(req, res, options)
+export default (req, res) => NextAuth(req, res, options);
